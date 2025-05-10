8d8b6b9faa1e1f8efa8928bf77e5900978900c0d
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2025-05-10 18:58:38 +0200

    chore: externalize vue

--- packages/config/src/external-dependencies.ts
+++ packages/config/src/external-dependencies.ts
@@ -1,2 +1,2 @@
-export const mifeExternalDependencies = ['single-spa', 'single-spa-react', 'single-spa-vue', 'react', 'react/jsx-runtime', 'react-dom', 'react-dom/client']
+export const mifeExternalDependencies = ['single-spa', 'single-spa-react', 'single-spa-vue', 'react', 'react/jsx-runtime', 'react-dom', 'react-dom/client', 'vue']
 export const rootExternalDependencies = ['single-spa']
