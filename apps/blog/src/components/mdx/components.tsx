e8cf1adff9f700a948b6f37722bd64ded37c7f92
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2025-05-20 16:51:38 +0200

    refactor: clean up code-block component

--- apps/blog/src/components/mdx/components.tsx
+++ apps/blog/src/components/mdx/components.tsx
@@ -1,4 +1,4 @@
-import { CodeBlock } from './code-block';
+import { CodeBlock } from './code-block/code-block';
 import { Error } from './error';
 import { H1, H2 } from './h1';
 import InlineCode from './inline-code';
