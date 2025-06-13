e4e5dbe9d60c7ea089e6b398e4715c833ea11aa2
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2025-06-13 08:35:26 +0200

    style: add panda css for custom styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Button, Flex, Text } from '@radix-ui/themes'
+import { css } from '../../styled-system/css'
+
+const textStyles = css({
+  color: 'red',
+})
 
 export default function Home() {
   return (
     <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
+      <Text className={textStyles}>Hello from Radix Themes :)</Text>
       <Button>Let&apos;s go</Button>
     </Flex>
   )
